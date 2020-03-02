1e794709a899af83ab6c2f1beecc575a66401c9a
slieman <slieman2001@hotmail.com>
2020-03-02 13:30:19 +0200

    updated...

--- learn-app/src/components/animationsTransitions/Index.vue
+++ learn-app/src/components/animationsTransitions/Index.vue
@@ -12,11 +12,20 @@
       </transition>
       <br>
        <h4>Slide with fade Transition</h4>
+            <button @click="numbers.push(10)">10</button>
+         <transition-group name="slide">
+           <li
+           class="list-group-item"
+           v-for="(number, index) in numbers"
+           @click="numbers.splice(index,1)"
+           style="cursor:pointer"
+           :key="number"
+           >
+            {{ number }}
+           </li>
+       </transition-group>
       <button class="btn btn-primary" @click="show2 = !show2">Show Alert</button>
       <br><br>
-      <!-- <transition-group>
-           
-       </transition-group> -->
         <transition name="slide" type="animation"> <!-- type = transition -->
           <div class="alert alert-info" v-if="show2">This is some info</div>
       </transition>
@@ -54,6 +63,7 @@
     @leave="leave"
     @after-leave="afterLeave"
     @leave-cancelled="leaveCancelled"
+    :css="false"
          >
           <div style="width:100px;height:100px;background-color:lightgreen;" v-if="show2">This is some info</div>
       </transition>
@@ -66,35 +76,50 @@ export default {
     data(){
         return {
             show:false,
-            show2:false
+            show2:false,
+            elementWidth: 100,
+            numbers:[1,2,4,6,7]
         }
     },
     methods:{
-        // beforeEnter(){
-        //     alert('beforeEnter')
-        // },
-        // enter(el,done){
-        //     alert('enter')
-        //     done();
-        // },
-        // afterEnter(){
-        //     alert('after enter')
-        // },
-        //  enterCancelled(){
-        //     alert('enterCancelled')
-        // },
-        // beforeLeave(){
-        //     alert('beforeLeave')
-        // },
-        // leave(){
-        //     alert('Leave')
-        // },
-        // afterLeave(){
-        //     alert('afterLeave')
-        // },
-        // leaveCancelled(){
-        //     alert('leaveCancelled')
-        // }
+        beforeEnter(el){
+            // alert('beforeEnter')
+            this.elementWidth = 100;
+            el.style.width  = this.elementWidth + 'px';
+        },
+        enter(el,done){
+            // alert('enter')
+            let round = 1;
+            const interval = setInterval(() => {
+                el.style.width = this.elementWidth  + round  * 10  + 'px';
+                round++;
+                if(round >20 ){
+                    clearInterval(interval);
+                    done()
+                }
+            }, 20);
+            done();
+        },
+        afterEnter(){
+            // alert('after enter')
+        },
+         enterCancelled(){
+            // alert('enterCancelled')
+        },
+        beforeLeave(el){
+            this.elementWidth = 100;
+            el.style.width = this.elementWidth + 'px';
+            // alert('beforeLeave')
+        },
+        leave(){
+            // alert('Leave')
+        },
+        afterLeave(){
+            // alert('afterLeave')
+        },
+        leaveCancelled(){
+            // alert('leaveCancelled')
+        }
     }
 }
 </script>
@@ -130,7 +155,12 @@ export default {
         animation: slide-out 1s ease-out forwards;
         transition: opacity 1s;
         opacity: 0;
+        position: absolute; /* solve bug animatation on ul li  */
         
+    }
+
+    .slide-move{
+        transition: transform 1s;
     }
 
     @keyframes slide-in {
